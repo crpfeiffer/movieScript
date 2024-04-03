5754b60a941933186785c0f47b0b207855986cf5
crpfeiffer <crpfeiffer@oakland.edu>
2024-04-03 12:40:29 -0400

    role view

--- roleJdbc.java
+++ roleJdbc.java
@@ -1,106 +1,57 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-import java.sql.*;
+public class Role extends HttpServlet {
 
-public class roleJdbc extends HttpServlet 
-{
+    // PreparedStatement for retrieving roles
+    private PreparedStatement pstmtSelect;
+  
+    // ... (other code including initializeJdbc)
+  
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException,IOException
-    {        
-			Statement state4 = null;
-			ResultSet result = null;
-			String query="";        
-			Connection con=null; 
-
-		try
-		{			
-            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver()); 
-            con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "project", "project");
-	       	System.out.println("Congratulations! You are connected successfully.");      
-     	}
-        catch(SQLException e)
-		{	
-			System.out.println("Error: "+e);	
-		}
-		catch(Exception e) 
-		{
-			System.err.println("Exception while loading  driver");		
-		}
-	    try 
-		{
-        	state4 = con.createStatement();
-		} 
-		catch (SQLException e) 	
-		{
-			System.err.println("SQLException while creating statement");			
-		}
-		
-		response.setContentType("text/html");
-		PrintWriter out = null ;
-		try
-		{
-			out =  response.getWriter();
-		}
-		catch (IOException e) 
-		{
-  			e.printStackTrace();
-		}
-		
-		query = "select m_title, m_date, m_length, cat_name, rating_name from movie, category, rating where movie.rating_id = rating.rating_id AND movie.cat_id = category.cat_id";
-		
-		out.println("<html><head><title>Moive Table Report</title>");	 
-		out.println("</head><body>");
-		
-		out.print( "<br /><b><center><font color=\"RED\"><H2>Movie Table Report</H2></font>");
-        out.println( "</center><br />" );
-       	try 
-		{ 
-			result=state4.executeQuery(query);
-	  	}
-		catch (SQLException e) 
-		{
-			System.err.println("SQLException while executing SQL Statement."); 
-		}
-		out.println("<center><table border=\"1\">"); 
-		out.println("<tr BGCOLOR=\"#cccccc\">");
-        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Title</td>");
-        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Release Date</td>");
-		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Duration</td>");
-        out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Category</td>");
-		out.println("<td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">Rating</td>");
+        throws ServletException, IOException {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+  
+      try {
+        // Retrieve roles (assuming no filtering for this example)
+        ResultSet rs = pstmtSelect.executeQuery();
+  
+        out.println("<h1>Movie Roles</h1>");
+        out.println("<table border=\"1\">");
+        out.println("<tr>");
+        out.println("<th>Movie ID</th>");
+        out.println("<th>Person ID</th>");
+        out.println("<th>Role ID</th>");
         out.println("</tr>");
-		try 
-		{ 
-            while(result.next()) 
-			{ 
-		    	out.println("<tr>");
-                out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(1)+"</td>");
-		    	out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(2)+"</td>");
-				out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(3)+"</td>");
-				out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(4)+"</td>");
-				out.println("     <td align = \"justify\"><font face =\"times new roman\"  size=\"4pt\">"+result.getString(5)+"</td>");
-                out.println("</tr>");
-			} 
-	    }
-		catch (SQLException e) 
-		{
-			System.out.println("Resutset is not connected"); 
-		}
-
-		out.println("</table></CENTER>");
-		try 
-		{ 
-   			result.close(); 
-			state4.close(); 	
-			con.close();
-    		System.out.println("Connection is closed successfully.");
- 	   	}
-		catch (SQLException e) 
-		{
-			e.printStackTrace();	
-		}
-
-  		out.println("</body></html>");
-    } 
-}
+  
+        // Iterate through results and display them
+        while (rs.next()) {
+          int retrievedMovieID = rs.getInt("movieID");
+          int retrievedPersonID = rs.getInt("personID");
+          int retrievedRoleID = rs.getInt("roleID");
+  
+          out.println("<tr>");
+          out.println("<td>" + retrievedMovieID + "</td>");
+          out.println("<td>" + retrievedPersonID + "</td>");
+          out.println("<td>" + retrievedRoleID + "</td>");
+          out.println("</tr>");
+        }
+  
+        out.println("</table>");
+      } catch (Exception ex) {
+        out.println("\n Error: " + ex.getMessage());
+      } finally {
+        out.close(); // Close stream
+      }
+    }
+  
+    private void initializeJdbc() {
+      // ... (Your database connection code)
+  
+      try {
+        // Prepare statement for selecting roles
+        pstmtSelect = conn.prepareStatement("SELECT r.movieID, r.personID, r.roleID FROM Role r");
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+  
